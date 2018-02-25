c0ecb2d317ad7bd2d7f8f1d7b436ae4a8c4107a9
therick96 <erick25gomez@gmail.com>
2018-02-25 18:58:41 -0400

    Creados enlaces a articulos

--- Proyecto_1/blog/views.py
+++ Proyecto_1/blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,7 @@ def post_list(request):
     posts = Post.objects.filter().order_by('publish_date')
     print posts
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post' : post})
