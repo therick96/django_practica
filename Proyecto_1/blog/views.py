ce3049816e65f6d45400858f88202f120edb83e4
therick96 <erick25gomez@gmail.com>
2018-02-25 18:02:41 -0400

    Añadida pagina post_list

--- Proyecto_1/blog/views.py
+++ Proyecto_1/blog/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter().order_by('publish_date')
+    print posts
+    return render(request, 'blog/post_list.html', {'posts' : posts})
