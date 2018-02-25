b80d4bf46f73ce1aa1cfcbda4d1dd0fa402d9dbd
therick96 <erick25gomez@gmail.com>
2018-02-25 15:30:44 -0400

    Añadido blog

--- Proyecto_1/primer_proyecto/settings.py
+++ Proyecto_1/primer_proyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #Añadida ruta para archivos estaticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #Anadida ruta para archivos estaticos
