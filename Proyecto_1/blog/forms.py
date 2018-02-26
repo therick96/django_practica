6023209c2ac4ebf9074d12e7e4929ef8c983a0e4
therick96 <erick25gomez@gmail.com>
2018-02-25 23:15:37 -0400

    Modificados los campos en el formulario

--- Proyecto_1/blog/forms.py
+++ Proyecto_1/blog/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        
+        super(PostForm, self).__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs = {
+            'class' : 'uk-input',
+        }
+        self.fields["text"].widget.attrs = {
+            'class' : "uk-textarea",
+            'rows' : 20,
+        }
+        
     class Meta:
         model = Post
-        fields = ("title", "text")
+        fields = ["title", "text"]
