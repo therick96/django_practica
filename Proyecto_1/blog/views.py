396af5141d08b640ea759eea1db0604f1a9b669e
therick96 <erick25gomez@gmail.com>
2018-02-25 20:41:52 -0400

    Creado formulario

--- Proyecto_1/blog/views.py
+++ Proyecto_1/blog/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,3 +16,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_new.html', {'form' : form})
