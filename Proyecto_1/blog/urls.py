e3d9b28b8240914f0232a2376094f6e489a15b56
therick96 <erick25gomez@gmail.com>
2018-02-25 22:02:32 -0400

    Modificar post

--- Proyecto_1/blog/urls.py
+++ Proyecto_1/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
     url(r'^new/$', views.post_new, name="post_new"),
+    url(r'^edit/(?P<pk>[0-9])/$', views.post_edit, name="post_edit")
 ]
